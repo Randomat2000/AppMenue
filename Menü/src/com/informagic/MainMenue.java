ca5be9e4badf89c6f5726464e822bc4a498332f4
Randomat2000 <dennis.neumann@hs.furtwangen.de>
2012-11-15 15:57:21 +0100

    Menue nur HM und Spielauswahl keine Bilder implementiert

---
+++ Menü/src/com/informagic/MainMenue.java
@@ -0,0 +1,20 @@
+package com.informagic;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainMenue extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menue, menu);
+        return true;
+    }
+}
